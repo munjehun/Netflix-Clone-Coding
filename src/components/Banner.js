217f68ce65c3fe1fb70cba19155e872fa18ec100
munjehun <miso1489@gmail.com>
2023-04-04 00:12:04 +0900

    배너 생성, 스타일 적용

---
+++ src/components/Banner.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function Banner({ popularMovie }) {
+  console.log(popularMovie);
+
+  return (
+    <div
+      className="banner"
+      style={{
+        backgroundImage:
+          'url(' +
+          `https://www.themoviedb.org/t/p/w1920_and_h800_multi_faces/${popularMovie.poster_path}` +
+          ')',
+      }}
+    >
+      <div className="banner-info">
+        <h1>{popularMovie.title}</h1>
+        <p>{popularMovie.overview}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Banner;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,26 @@
 import React, { useEffect } from 'react';
 import { movieActions } from '../redux/actions/movieActions';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import Banner from '../components/Banner';
 
 function Home() {
   const dispatch = useDispatch();
+  const { popularMovies, topRatedMovies, upcomingMovies } = useSelector(
+    (state) => state.movie
+  );
 
+  // ⚠️useEffect는 렌더 후 호출되므로 주의!
   useEffect(() => {
     dispatch(movieActions.getMovies());
   }, []);
-  return <div>Home</div>;
+
+  return (
+    <div>
+      {popularMovies.results && (
+        <Banner popularMovie={popularMovies.results[0]} />
+      )}
+    </div>
+  );
 }
 
 export default Home;
